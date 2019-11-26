ca2702ad32bdff867db853dd185c281a8847c318
Radko Stanev <radkostanev@gmail.com>
2019-11-26 18:52:35 +0200

    image name included now

--- CocktailWizard/CocktailWizard.Web/wwwroot/js/ModalCocktailsDeleteConfirm.js
+++ CocktailWizard/CocktailWizard.Web/wwwroot/js/ModalCocktailsDeleteConfirm.js
@@ -2,7 +2,7 @@
     $(document).on('click', '.delete-item', function (e) {
         e.preventDefault();
 
-        var form = $(this).closest('form');
+       let form = $(this).closest('form');
         bootbox.confirm("Are you sure you want to delete this cocktail?", function (result) {
             if (result === true) {
                 form.trigger('submit');
---
+++ CocktailWizard/CocktailWizard.Web/wwwroot/js/PreviewFileImage.js
@@ -0,0 +1,5 @@
+﻿$('input[type="file"]').change(function (e) {
+    let fileName = e.target.files[0].name;
+    $('#custom-file-label').append(fileName);
+    console.log(fileName)
+});
